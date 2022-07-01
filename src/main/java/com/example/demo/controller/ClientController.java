69ad099c09d7e9bf380715496bec15aa0b0285c0
manumura <emmanuel.mura@gmail.com>
2022-07-01 21:34:35 +0700

    account service

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @AllArgsConstructor
@@ -19,9 +21,30 @@ public class ClientController {
     final AccountService accountService;
 
     @GetMapping("/api/accounts/{name}")
-    public ResponseEntity<Account> getAuthor(@PathVariable String name) {
+    public ResponseEntity<Account> getOneByName(@PathVariable String name) {
         try {
             return ResponseEntity.ok(accountService.getOneByName(name));
+        } catch (StatusRuntimeException e) {
+            log.error(e.getMessage(), e);
+            if (Status.NOT_FOUND.equals(e.getStatus())) {
+                return ResponseEntity.notFound().build();
+            }
+            if (Status.FAILED_PRECONDITION.equals(e.getStatus())) {
+                return ResponseEntity.badRequest().build();
+            }
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/api/accounts")
+    public ResponseEntity<List<Account>> getAll() {
+        try {
+            return ResponseEntity.ok(accountService.getAll());
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            // Restore interrupted state...
+            Thread.currentThread().interrupt();
+            return ResponseEntity.internalServerError().build();
         } catch (StatusRuntimeException e) {
             log.error(e.getMessage(), e);
             if (Status.NOT_FOUND.equals(e.getStatus())) {
--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,13 +1,21 @@
 package com.example.demo.service;
 
 import com.example.demo.account.AccountServiceGrpc;
+import com.example.demo.account.GetAllRequest;
 import com.example.demo.account.GetOneByNameRequest;
 import com.example.demo.dto.Account;
 import com.example.demo.mapper.Mapper;
-import com.example.demo.util.ConvertionUtils;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -16,10 +24,43 @@ public class AccountService {
     @GrpcClient("account-service")
     private AccountServiceGrpc.AccountServiceBlockingStub synchronousAccountServiceClient;
 
+    @GrpcClient("account-service")
+    private AccountServiceGrpc.AccountServiceStub asynchronousAccountServiceClient;
+
     public Account getOneByName(String name) {
         com.example.demo.account.Account account = synchronousAccountServiceClient.getOneByName(GetOneByNameRequest.newBuilder()
                 .setName(name)
                 .build());
         return Mapper.buildAccountResponse(account);
+    }
+
+    public List<Account> getAll() throws InterruptedException {
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        final List<Account> accounts = new ArrayList<>();
+        asynchronousAccountServiceClient.getAll(GetAllRequest.newBuilder().build(),
+                new StreamObserver<>() {
+                    @Override
+                    public void onNext(com.example.demo.account.Account account) {
+                        log.info("Next: {}", account);
+                        accounts.add(Mapper.buildAccountResponse(account));
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Status status = Status.fromThrowable(throwable);
+                        log.warn("Error: {}", status);
+                        finishLatch.countDown();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        log.info("Completed");
+                        finishLatch.countDown();
+                    }
+                });
+
+        // Receiving happens asynchronously
+        boolean await = finishLatch.await(1, TimeUnit.MINUTES);
+        return await ? accounts : Collections.emptyList();
     }
 }
